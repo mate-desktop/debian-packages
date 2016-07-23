ec330cb283a1b42907bf65e483d20d42bed4ce3f
Matt Spaulding <mspaulding06@gmail.com>
2016-07-23 14:12:08 -0700

    Make dep installation an option

--- build.py
+++ build.py
@@ -10,6 +10,7 @@ import sys
 parser = optparse.OptionParser()
 parser.add_option("-s", "--source", dest="source", action="store_true", help="Build source package")
 parser.add_option("-i", "--i386", dest="i386", action="store_true", help="Build i386 binary package")
+parser.add_option("-d", "--install-deps", dest="install_deps", action="store_true", help="Install development dependencies")
 (options, args) = parser.parse_args()
 
 if len(args) == 0:
@@ -44,9 +45,10 @@ for i in range(0, len(args)):
     os.system("echo '3.0 (native)' > debian/source/format")
 
     # discover and install build dependencies
-    os.system("mk-build-deps")
-    os.system("sudo dpkg -i *.deb")
-    os.system("sudo apt-get install -y -f")
+    if options.install_deps:
+        os.system("mk-build-deps")
+        os.system("sudo dpkg -i *.deb")
+        os.system("sudo apt-get install -y -f")
 
     # debian/changelog
     date = datetime.date.today().strftime("%Y%m%d")
--- build.sh
+++ build.sh
@@ -11,13 +11,13 @@ rm -rf deb logs installed
 mkdir installed
 
 for pkg in $PACKAGES; do
-  ./debian-packages/build.py $pkg
+  ./debian-packages/build.py --install-deps $pkg
   sudo dpkg -i ./deb/*.deb
   mv ./deb/*.deb ./installed/
 done
 
 for pkg in $EXTRA_PACKAGES; do
-  ./debian-packages/build.py $pkg
+  ./debian-packages/build.py --install-deps $pkg
   sudo dpkg -i ./deb/*.deb
   mv ./deb/*.deb ./installed/
 done
